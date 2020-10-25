1c9231c5881b29d0840cabfba01293f8a80c1ab5
tomas <128593@student.drenthecollege.nl>
2020-10-25 23:14:33 +0100

    New commit

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
-
+function myFunction() {
+    if (document.getElementsByClassName("form-group" != "undefined")) {
+        if (confirm("Do you want to sign up?")) {
+            console.log("Sign up succes!");
+        }
+    } else {
+        console.log("Sign up failed!");
+    }
+}
